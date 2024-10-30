832fbaef4f630cd6a69156ab297d569fed441232
Sm1le <onekonstantin@gmail.com>
2024-10-30 14:36:37 +0300

    2 buttons upload photo

--- src/components/InputFile/InputFile.tsx
+++ src/components/InputFile/InputFile.tsx
@@ -11,6 +11,7 @@ interface InputFileProps {
 const InputFile: React.FC<InputFileProps> = ({ imageUrl, onFileUpload }) => {
   const [loading, setLoading] = useState<boolean>(false);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const fileInputCameraRef = useRef<HTMLInputElement | null>(null);
 
   const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
@@ -35,18 +36,31 @@ const InputFile: React.FC<InputFileProps> = ({ imageUrl, onFileUpload }) => {
     fileInputRef.current?.click();
   };
 
+  const handleButtonCameraClick = () => {
+    fileInputCameraRef.current?.click();
+  };
+
   return (
     <div className={styles.inputFile}>
       <input
         type="file"
-        // capture="environment"
         ref={fileInputRef}
         className={styles.input}
         accept="image/*"
         onChange={handleChange}
         disabled={loading}
       />
+      <input
+        type="file"
+        capture="environment"
+        ref={fileInputCameraRef}
+        className={styles.input}
+        accept="image/*"
+        onChange={handleChange}
+        disabled={loading}
+      />
       <Button type="button" arrow={false} disabled={loading} text="Выбрать изображение" onClick={handleButtonClick} />
+      <Button type="button" arrow={false} disabled={loading} text="Сделать фото" onClick={handleButtonCameraClick} />
       {loading && <div className={styles.loader}>Загрузка...</div>}
       {imageUrl !== "" && (
         <div className={styles.imageContainer}>
