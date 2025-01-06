a7b585c4b7792174d5441024d0a6240a7f9798cf
Sm1le <onekonstantin@gmail.com>
2025-01-06 16:44:27 +0300

    edit name

--- src/components/CardButtonsContainer/CardButtonsContainer.tsx
+++ src/components/CardButtonsContainer/CardButtonsContainer.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
 import styles from "./CardButtonsContainer.module.scss";
-import { MdOutlineDelete, MdDone, MdClose } from "react-icons/md";
+import { MdOutlineDelete, MdDone, MdClose, MdEdit } from "react-icons/md";
 import { ButtonIcon } from "../ButtonIcon";
 import { TVodkaCollection } from "@/firebase/saveDataToFirebase";
 import { deleteDataFromFirebase } from "@/firebase/deleteDataFromFirebase";
 import { deleteImgFromFirebase } from "@/firebase/deleteImgFromFirebase";
+import { EditNameModal } from "../EditNameModal";
 
 interface CardButtonsContainerProps {
   vodka: TVodkaCollection;
@@ -14,6 +15,7 @@ interface CardButtonsContainerProps {
 
 const CardButtonsContainer: React.FC<CardButtonsContainerProps> = ({ vodka, setCollectionIsChanged }) => {
   const [deleteModal, setDeleteModal] = useState(false);
+  const [editNameModal, setEditNameModal] = useState(false);
 
   const handleButtonDeleteClick = async (id: string) => {
     console.log("Удалить водку с id:", vodka.id, "url:", vodka.imageUrl);
@@ -25,6 +27,16 @@ const CardButtonsContainer: React.FC<CardButtonsContainerProps> = ({ vodka, setC
 
   return (
     <div className={styles.cardButtonsContainer}>
+      <ButtonIcon icon={MdEdit} onClick={() => setEditNameModal(true)} />
+      {editNameModal && (
+        <EditNameModal
+          id={vodka.id}
+          name={vodka.name}
+          editNameModal={editNameModal}
+          setEditNameModal={setEditNameModal}
+          setCollectionIsChanged={setCollectionIsChanged}
+        />
+      )}
       <ButtonIcon icon={MdOutlineDelete} onClick={() => setDeleteModal(true)} />
       {deleteModal && (
         <div className={styles.deleteModal}>
